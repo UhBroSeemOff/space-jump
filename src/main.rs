3f50ece78d565a935b17a55f54027c956d981adf
UhBroSeemOff <fantomklin@outlook.com>
2023-06-22 20:16:34 +0600

    Added game and primary window initialization

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use bevy::{prelude::*, window::*};
+
+const RESOLUTION: Vec2 = Vec2 {
+    x: 1920.0,
+    y: 1080.0,
+};
+
+const TITLE: &str = "Space Jump";
+
 fn main() {
-    println!("Hello, world!");
+    let default_plugin = configure_default_plugin();
+    App::new().add_plugins(default_plugin).run();
+}
+
+fn configure_default_plugin() -> bevy::app::PluginGroupBuilder {
+    let window_plugin = initialize_window_plugin();
+    DefaultPlugins.set(window_plugin)
+}
+
+fn initialize_window_plugin() -> WindowPlugin {
+    WindowPlugin {
+        primary_window: Some(initialize_primary_window()),
+        ..default()
+    }
+}
+
+fn initialize_primary_window() -> Window {
+    Window {
+        mode: WindowMode::BorderlessFullscreen,
+        title: TITLE.to_owned(),
+        resolution: WindowResolution::new(RESOLUTION.x, RESOLUTION.y),
+        ..default()
+    }
 }
