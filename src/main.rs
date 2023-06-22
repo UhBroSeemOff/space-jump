d418201b8ec8c7d5b8e5d55ac9d0c60b89e20c3b
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-06-23 02:08:17 +0400

    fixed project name in toml file, added application state, added app initializer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use bevy::{prelude::*, window::WindowMode};
+
 fn main() {
     println!("Hello, world!");
+
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                mode: WindowMode::Fullscreen,
+                title: "Space jump".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_state::<ApplicationState>();
+}
+
+#[derive(States, Debug, Clone, Copy, Eq, PartialEq, Hash, Default)]
+pub enum ApplicationState {
+    #[default]
+    MainMenu,
+    Game,
+    GameOver,
+    PauseMenu,
 }
