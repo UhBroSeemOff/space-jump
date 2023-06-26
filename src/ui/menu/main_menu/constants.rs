84e14e6c5e015ea01ce8d092e4741ce26a34ada5
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-06-26 19:56:59 +0400

    fixed all game states, added constant values for buttons and default style function

---
+++ src/ui/menu/main_menu/constants.rs
@@ -0,0 +1,20 @@
+use bevy::{prelude::*, text::TextStyle};
+
+// TODO: поменять цвета
+pub const NORMAL_BUTTON_COLOR: Color = Color::rgb(1.0, 1.0, 1.0);
+pub const PRESSED_BUTTON_COLOR: Color = Color::rgb(0.35, 0.75, 0.35);
+
+pub fn get_text_style(asset_server: &Res<AssetServer>, font_size: f32) -> TextStyle {
+    TextStyle {
+        font: asset_server.load("fonts/CyrillicPixel.ttf"),
+        font_size: font_size,
+        color: Color::WHITE,
+    }
+}
+
+pub const DEFAULT_BUTTON_STYLE: Style = Style {
+    justify_content: JustifyContent::Center,
+    align_items: AlignItems::Center,
+    size: Size::new(Val::Px(280.0), Val::Px(80.0)),
+    ..Style::DEFAULT
+};
