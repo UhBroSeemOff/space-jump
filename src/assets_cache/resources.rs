1de7ce860ddcd8001afa9e396a9a713778be85ad
Alexander Inkeev <43778666+descrybe@users.noreply.github.com>
2023-06-27 18:22:29 +0300

    Added assets cache  (#13)
    * created asset cache
    
    * added asset cache plugin to main func
    
    * renamed font asset
    
    ---------
    
    Co-authored-by: alexander_inkeev <alexander.inkeev@unrvld.com>

---
+++ src/assets_cache/resources.rs
@@ -0,0 +1,41 @@
+use bevy::{
+    prelude::{AssetServer, FromWorld, Handle, Image, Resource},
+    text::Font,
+};
+
+#[derive(Resource)]
+pub struct AssetsCache {
+    pub sprites: Sprites,
+    pub fonts: Fonts,
+}
+
+pub struct Fonts {
+    pub main: Handle<Font>,
+}
+
+pub struct Sprites {
+    pub entities: EntitiesSprites,
+    pub characters: CharactersSprites,
+    pub ui: UISprites,
+}
+
+pub struct UISprites {}
+pub struct EntitiesSprites {}
+pub struct CharactersSprites {}
+
+impl FromWorld for AssetsCache {
+    fn from_world(world: &mut bevy::prelude::World) -> Self {
+        let assets_loader = world.get_resource::<AssetServer>().unwrap();
+
+        return AssetsCache {
+            fonts: Fonts {
+                main: assets_loader.load("fonts/CyrillicPixel.ttf"),
+            },
+            sprites: Sprites {
+                characters: CharactersSprites {},
+                ui: UISprites {},
+                entities: EntitiesSprites {},
+            },
+        };
+    }
+}
