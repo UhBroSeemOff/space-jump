ad8e23584f62020756539d47970457c8ea541b89
DolphinNine <66217086+DolphinNine@users.noreply.github.com>
2023-06-29 01:35:21 +0700

    Implement player entity (#21)
    1) Add player entity;
    2) Add player spawn;
    3) Implement camera 'follow_target' system;
    4) Set camera to follow player

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -1,13 +1,15 @@
 use bevy::prelude::Plugin;
 
-use self::systems::setup_camera;
+use self::systems::{follow_target, setup_camera};
 
+pub(crate) mod components;
 mod systems;
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
-        app.add_startup_system(setup_camera);
+        app.add_startup_system(setup_camera)
+            .add_system(follow_target);
     }
 }
--- src/entities/mod.rs
+++ src/entities/mod.rs
@@ -1,12 +1,17 @@
+use bevy::{app::PluginGroupBuilder, prelude::PluginGroup};
+
 pub mod asteroid;
+pub mod player;
 
 use asteroid::AsteroidPlugin;
-use bevy::{app::PluginGroupBuilder, prelude::PluginGroup};
+use player::PlayerPlugin;
 
 pub struct EntitiesPlugins;
 
 impl PluginGroup for EntitiesPlugins {
     fn build(self) -> PluginGroupBuilder {
-        PluginGroupBuilder::start::<Self>().add(AsteroidPlugin)
+        PluginGroupBuilder::start::<Self>()
+            .add(AsteroidPlugin)
+            .add(PlayerPlugin)
     }
 }
