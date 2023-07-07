dd77c192b61a1219596d963672a8a1e8091f6c2d
alexander_inkeev <alexander.inkeev@unrvld.com>
2023-07-07 21:25:06 +0400

    added menu background function

--- src/ui/menu/main_menu/systems.rs
+++ src/ui/menu/main_menu/systems.rs
@@ -1,21 +1,53 @@
 use bevy::app::AppExit;
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 
 use crate::resources::ApplicationState;
 
-use super::components::*;
 use super::super::super::constants::*;
+use super::components::*;
 
-pub fn render_menu_background() {
-    println!("Spawn menu background");
+pub fn render_menu_background(
+    mut commands: Commands,
+    asset_server: &Res<AssetServer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
+    let texture_handle = asset_server.load("sprites/menu_background.jpg");
+    let window = window_query.get_single().unwrap();
+
+    commands.spawn((
+        SpriteBundle {
+            texture: texture_handle.into(),
+            sprite: Sprite {
+                custom_size: Option::Some(Vec2::new(window.width(), window.height())),
+                ..default()
+            },
+            transform: Transform::from_xyz(window.width() / 2.0, window.height() / 2.0, 1.0),
+            ..default()
+        },
+        MainMenuBackground {},
+    ));
+}
+
+pub fn destroy_menu_background(
+    mut commands: Commands,
+    mut bg_query: Query<Entity, With<MainMenuBackground>>,
+) {
+    let menu_bg = bg_query.get_single().unwrap();
+    commands.entity(menu_bg).despawn();
 }
 
 pub fn add_menu_music() {
     println!("Start some music");
 }
 
-pub fn render_menu_ui(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn render_menu_ui(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    window_query: Query<&Window, With<PrimaryWindow>>,
+) {
     main_menu_setup(&mut commands, &asset_server);
+    render_menu_background(commands, &asset_server, window_query);
 }
 
 fn get_button_bundle(texture_handle: Handle<Image>) -> ButtonBundle {
@@ -43,9 +75,6 @@ fn get_text_bundle(button_text: &str, asset_server: &Res<AssetServer>) -> TextBu
         ..default()
     }
 }
-
-// TODO: Сделать функцию spawn_button, вынести её в сервисы
-// понять, как выносить компонент, связующий бандл в аргумент monkaHmm
 
 fn spawn_play_button(
     object: &mut ChildBuilder,
@@ -114,7 +143,7 @@ pub fn main_menu_setup(commands: &mut Commands, asset_server: &Res<AssetServer>)
                         text: Text {
                             sections: vec![TextSection::new(
                                 "Space jump",
-                                get_text_style(asset_server, 60.0, TITLE_TEXT_COLOR),
+                                get_text_style(asset_server, 90.0, TITLE_TEXT_COLOR),
                             )],
                             alignment: TextAlignment::Center,
                             ..default()
@@ -136,10 +165,6 @@ pub fn main_menu_setup(commands: &mut Commands, asset_server: &Res<AssetServer>)
         .id();
 
     return main_menu_entity;
-}
-
-pub fn destroy_menu_background() {
-    println!("Destroy menu background");
 }
 
 pub fn remove_menu_music() {
