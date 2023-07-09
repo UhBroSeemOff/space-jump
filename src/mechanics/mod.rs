fd71673671f95019afd7d3db7c031b7c372e06e9
DolphinNine <dn.nine@gmail.com>
2023-07-09 18:08:34 +0700

    Remove projectile plugin

--- src/mechanics/mod.rs
+++ src/mechanics/mod.rs
@@ -2,12 +2,10 @@ use bevy::{app::PluginGroupBuilder, prelude::PluginGroup};
 
 pub mod projectile;
 
-use projectile::ProjectilePlugin;
-
 pub struct MechanicsPlugins;
 
 impl PluginGroup for MechanicsPlugins {
     fn build(self) -> PluginGroupBuilder {
-        PluginGroupBuilder::start::<Self>().add(ProjectilePlugin)
+        todo!()
     }
 }
