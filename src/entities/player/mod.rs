9cfd545408b3d7fbe24e5ee8bb5b1d93f34ef535
DolphinNine <66217086+DolphinNine@users.noreply.github.com>
2023-08-06 01:23:50 +0700

    Implement basic player's "jump" acceleration (#34)
    1) Add player's 'jump' system;
    2) Tweak player's physics parameters.

--- src/entities/player/mod.rs
+++ src/entities/player/mod.rs
@@ -7,7 +7,7 @@ mod systems;
 use crate::resources::ApplicationState;
 
 use self::events::{PlayerSpawnedEvent, SpawnPlayerEvent};
-use self::systems::{setup, spawn_player};
+use self::systems::{jump, setup, spawn_player};
 
 pub struct PlayerPlugin;
 
@@ -15,6 +15,7 @@ impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut bevy::prelude::App) {
         app.add_system(setup.in_schedule(OnEnter(ApplicationState::Game)))
             .add_system(spawn_player)
+            .add_system(jump)
             .add_event::<SpawnPlayerEvent>()
             .add_event::<PlayerSpawnedEvent>();
     }
